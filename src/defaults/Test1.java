92f9ceb53a8d5a7d05ce5f36f2afb087557585a9
SAMEER <SAMEER@LAPTOP-B8C5R7AU>
2022-09-22 16:37:39 +0530

    First commit...

---
+++ src/defaults/Test1.java
@@ -0,0 +1,16 @@
+package defaults;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry = "Yes";
+		if(hungry ==  "yes") 
+		System.out.println("eat");
+		else
+			System.out.println("eat");
+		
+	}
+
+}
